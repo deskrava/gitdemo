4124a5fe3978549c07202a5ad3de0575f38d5950
DESKRAVA <disy8828@ya.ru>
2022-01-06 11:18:00 +0300

    Initial Commit

---
+++ gitdemo/gitdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitdemoApp.swift
+//  gitdemo
+//
+//  Created by Денис Кравченко on 06.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct gitdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
